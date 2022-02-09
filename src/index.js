829a06c43e4308b0f2d5931b8d4014786bddc45f
Sandeep Reddy <sandeepreddy4366@gmail.com>
2022-02-09 12:37:53 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+//css
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Tittle />
+      <Author />
+    </article>
+  )
+}
+const Image = () => (
+  <img
+    src='https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg'
+    alt=''
+  />
+)
+const Tittle = () => <h1>The Japanese secret to a long and happy life </h1>
+const Author = () => (
+  <h4 style={{ color: '#61798', fontSize: '0.75rem', marginTop: '0.25ren' }}>
+    Francesc Miralles
+  </h4>
+)
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
