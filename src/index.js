c2b018022fedd00d09acb7e00cb601bebcf71220
Sandeep Reddy <sandeepreddy4366@gmail.com>
2022-02-10 17:45:58 +0530

    basic project

--- src/index.js
+++ src/index.js
@@ -1,38 +1,57 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom'
 //css
 import './index.css'
 
+const firstBook = {
+  img:'https://images-eu.ssl-images-amazon.com/images/I/41FIdMzjGUL._AC_SX184_.jpg',
+  tittle:'The Japanese secret to a long and happy life',
+  author:'Francesc Miralles',
+
+};
+const secondBook = {
+  img:'https://m.media-amazon.com/images/I/81zz6LqCreS._AC_UY327_FMwebp_QL65_.jpg',
+  tittle:'How to Stop Worrying and Start Living: Time-Tested Methods for Conquering Worry',
+  author:'Dale Carnegie',
+
+};
+
+
+
 function BookList() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  )
+    <section className='booklist'>
+      <Book 
+      img={firstBook.img}
+      tittle={firstBook.tittle}
+      author={firstBook.author}
+      > 
+  
+   </Book>
+   
+   <Book 
+   img={secondBook.img}
+   tittle={secondBook.tittle}
+   author={secondBook.author}
+   />
+   </section>
+  );
 }
+  
+   
+const Book = (props) => {
+  const {  img,  tittle,    author,   Children   } = props;
 
-const Book = () => {
+
   return (
-    <article>
-      <Image></Image>
-      <Tittle />
-      <Author />
+    <article className='book'>
+    <img src={img}alt=''/>
+    <h1>{tittle}</h1>
+    <h4>{author}</h4>
+    {Children}
     </article>
-  )
-}
-const Image = () => (
-  <img
-    src='https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg'
-    alt=''
-  />
-)
-const Tittle = () => <h1>The Japanese secret to a long and happy life </h1>
-const Author = () => (
-  <h4 style={{ color: '#61798', fontSize: '0.75rem', marginTop: '0.25ren' }}>
-    Francesc Miralles
-  </h4>
-)
+  );
+  
+};
 
-ReactDOM.render(<BookList />, document.getElementById('root'))
+ReactDOM.render(<BookList />, document.getElementById('root'));
